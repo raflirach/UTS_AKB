9daa9d38d5372e2e8df8e39e71ed375d55fc3813
raflirach <rafli060395@email.unikom.ac.id>
2019-05-17 06:04:06 +0700

    add Splash screen

--- app/src/main/java/com/example/uts_akb/MainActivity.java
+++ app/src/main/java/com/example/uts_akb/MainActivity.java
@@ -2,12 +2,18 @@ package com.example.uts_akb;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.LinearLayout;
+
+import com.wang.avi.AVLoadingIndicatorView;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
